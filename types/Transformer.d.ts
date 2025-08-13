c732713a026bc2830912d1d7356a25b1ff57c3bf
ЯRаСлав <support@yaro.page>
2025-08-13 22:29:59 +0300

    Types updated.

---
+++ types/Transformer.d.ts
@@ -0,0 +1,41 @@
+export default XMLTransformer;
+/**
+ * Class representing an XML transformer.
+ * Encodes nano objects to XML format, and decodes XML strings to nano objects.
+ * @extends Transformer
+ */
+declare class XMLTransformer extends Transformer {
+    /**
+     * Creates a new XMLTransformer instance.
+     * @param {Object} [options={}] - Options for HTML conversion.
+     * @param {string} [options.tab='\t'] - The string to use for indentation.
+     * @param {string} [options.eol='\n'] - The string to use for new lines.
+     * @param {XMLTags} [options.defaultTags] - The default tag mappings for conversion.
+     */
+    constructor(options?: {
+        tab?: string | undefined;
+        eol?: string | undefined;
+        defaultTags?: XMLTags | undefined;
+    } | undefined);
+    /** @type {string} */
+    tab: string;
+    /** @type {string} */
+    eol: string;
+    /** @type {XMLTags} */
+    defaultTags: XMLTags;
+    /**
+     * Encodes a nano object to HTML format.
+     * @param {Object|Array} data - The nano object or array to encode.
+     * @returns {Promise<string>} - The HTML string representation.
+     */
+    encode(data: any | any[]): Promise<string>;
+    /**
+     * Decodes an XML string to a nano object.
+     * Note: This method is a placeholder and should be implemented with actual HTML parsing logic.
+     * @param {string} str - The XML string to decode.
+     * @returns {Promise<Object|Array>} - The decoded nano object or array.
+     */
+    decode(str: string): Promise<any | any[]>;
+}
+import Transformer from "@nan0web/transformer";
+import XMLTags from "./XMLTags.js";
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,9 @@
-export default nano2xml;
+export default XMLTransformer;
 import Case from "./Case.js";
 import escape from "./escape.js";
 import nano2attrs from "./nano2attrs.js";
 import nano2xml from "./nano2xml.js";
 export const defaultXMLTags: XMLTags;
 import XMLTags from "./XMLTags.js";
-export { Case, escape, nano2attrs, nano2xml, XMLTags };
+import XMLTransformer from "./Transformer.js";
+export { Case, escape, nano2attrs, nano2xml, XMLTags, XMLTransformer };
